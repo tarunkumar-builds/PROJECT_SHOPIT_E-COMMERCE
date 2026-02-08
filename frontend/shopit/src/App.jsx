526e067d8d8ac7b9923bda306bf04c5e5f658300
tarunkumar-builds <itarunkumar2803@gmail.com>
2026-02-08 16:17:50 +0530

    Fix import case for Verify component

--- frontend/shopit/src/App.jsx
+++ frontend/shopit/src/App.jsx
@@ -14,7 +14,7 @@ import SearchBar from "./components/SearchBar";
 import { ShopContextProvider } from "./context/ShopContext";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Verify } from "./pages/verify";
+import { Verify } from "./pages/Verify";
 
 
 export default function App() {
